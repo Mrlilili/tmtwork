f3e27d0e2cc01edbae3753de405586fe7b39f373
Aaron <15360085185>
2016-07-01 16:53:10 +0800

    save

--- _tasks/webpack.config.js
+++ _tasks/webpack.config.js
@@ -16,5 +16,12 @@ module.exports = {
     entry:entryObj,
     output: {
         filename: '[name].js'
-    }
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: "style-loader!css-loader"},
+            {test: /\.png$/, loader: "url-loader?limit=100000"},
+            {test: /\.jpg$/, loader: "file-loader"}
+        ]
+    },
 }
--- test/src/js/index.js
+++ test/src/js/index.js
@@ -2,5 +2,6 @@
  * Created by admin-b on 2016/6/27.
  * fas
  */
-require('./lib/t1')
+require('./lib/t1');
+require('../css/style-b.css');
 console.log('123465');
